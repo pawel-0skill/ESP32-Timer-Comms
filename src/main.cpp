352277f44ed32d47e5fde702d59a004b89c72ffb
pawel-0skill <124441221+pawel-0skill@users.noreply.github.com>
2023-07-30 21:40:26 +0200

    Got Comms to read Gate messages, great success.
    But that MAC hardcoding thing, man...

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,51 @@
 /*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+  Thank you Rui Santos https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
 */
 
-#include <esp_now.h>
+#include <Arduino.h>
 #include <WiFi.h>
+#include <esp_now.h>
 
-// Structure example to receive data
-// Must match the sender structure
+// No Comms address yet - but we will set two later, to ping Gates
+
+// Define data structure - we will receive either PING or EVENT 
 typedef struct struct_message {
-    char a[32];
-    int b;
-    float c;
-    bool d;
+  char a[32];
 } struct_message;
+struct_message gateMessage;
 
-// Create a struct_message called myData
-struct_message myData;
+// Not adding that magical bit, i guess we're just receiving so we dont need it
+//esp_now_peer_info_t peerInfo;
 
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&myData, incomingData, sizeof(myData));
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  Serial.println(macStr);
+  memcpy(&gateMessage, incomingData, sizeof(gateMessage));
   Serial.print("Bytes received: ");
   Serial.println(len);
-  Serial.print("Char: ");
-  Serial.println(myData.a);
-  Serial.print("Int: ");
-  Serial.println(myData.b);
-  Serial.print("Float: ");
-  Serial.println(myData.c);
-  Serial.print("Bool: ");
-  Serial.println(myData.d);
+  Serial.print("Content: ");
+  Serial.println(gateMessage.a);
   Serial.println();
 }
  
 void setup() {
-  // Initialize Serial Monitor
+  // TODO: LED init
+
+  // Serial init
   Serial.begin(9600);
   
-  // Set device as a Wi-Fi Station
+  // Wifi init, WiFi station, ESP-NOW init
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
+    // TODO later - LED feedback
   }
   
   // Once ESPNow is successfully Init, we will register for recv CB to
